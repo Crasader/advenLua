03c681c6ed2ab3a36ed4887c3dd792c3f7a08c06
sadi <272682545@qq.com>
2015-09-18 18:08:32 +0800

    增加玩家对象数组

--- Server/Mutiplay.js
+++ Server/Mutiplay.js
@@ -1,6 +1,6 @@
 var WebSocketServer = require('ws').Server;
 
-var wss = new WebSocketServer({port: 3000});
+var wss = new WebSocketServer({port: 4000});
 
 var fs = require('fs');
 
@@ -8,15 +8,34 @@ console.log('Server on port 3000.');
 
 var fs = require('fs');
 
-wss.on('connection', function(ws){
+//创建一个对象构造器来构建一个人物对象
+function Person (id) {
+	this.id = id;
+}
 
+var people = new Array();
+var numOfConnect = 0;
+
+
+wss.on('connection', function(ws){
+	//每次连接加1
+	numOfConnect = numOfConnect + 1;
+	people[numOfConnect] = new Person(numOfConnect);
+	console.log(people[numOfConnect].id);
 	//register to receive message
-	ws.on('message', function(data){
-			console.log(data);
-			wss.clients.forEach(function each (client) {
-				//广播数据
-				client.send(data);
-			})
+	ws.on('message', function(data)
+	{
+		console.log(data);
+		wss.clients.forEach(function each (client) 
+		{
+			//广播数据
+			client.send(data);
 		});
 	});
+});
+
+wss.on('close', function  () {
+	console.log("close");
+	people[numOfConnect] = null;
+	numOfConnect = numOfConnect - 1;
 });
