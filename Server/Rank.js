2ddb0842a32c4ed88d97be4c2b277315935a5f77
zengzhining <272682545@qq.com>
2015-09-28 21:45:31 +0800

    更新服务器代码，不重复玩家，并且实现本地保存

--- Server/Rank.js
+++ Server/Rank.js
@@ -9,24 +9,27 @@ console.log('Server on port 4000.');
 var fs = require('fs');
 
 //创建一个对象构造器来构建一个人物对象
-function Person (name, score) {
+function Person (name, score) 
+{
 	this.Name = name;
 	this.Score = score;
 }
 
-var peopleScore = new Array(100, 200, 300, 400, 500);
-var peopleName = new Array("CCC", "BBB", "AAA", "DDD", "EEE");
+var peopleScore = new Array(600, 500, 400, 300, 200, 100);
+var peopleName = new Array("CCC", "BBB", "AAA", "DDD", "EEE", "FFF");
 
-var Score = new Array(6);
+//这个时候读取文件
+// var fileStr = fs.readFileSync("./")
+var str = fs.readFileSync("./Score.json");
+var Score = JSON.parse(str)
 
-//初始化
-for (i = 0; i < Score.length; i ++)
+console.log("the Score is :");
+for (var i = Score.length - 1; i >= 0; i--) {
+	console.log(Score[i]);
+};
+
+wss.on('connection', function(ws)
 {
-	console.log(num);
-	Score[i] = new Person(peopleName[i], peopleScore[i]);
-}
-
-wss.on('connection', function(ws){
 	//获取数据
 	// var str = fs.readFileSync("./Score.json");
 
@@ -46,8 +49,38 @@ wss.on('connection', function(ws){
 			Score[b] = temp;
 		}
 
-		function sortAll ( obj) {
-			Score[5] = obj;
+		function isNeedSort (obj)
+		{
+			for (var num = 0; num < Score.length; num ++)
+			{
+				//先判断是不是同一个玩家，如果是就比较其分数有没超越，没有就返回
+				if (Score[num].Name == obj.Name )
+				{
+					if(Score[num].Score <= obj.Score)
+					{
+						Score[num] = obj;
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+
+			if ( obj.Score >= Score[4].Score )
+			{
+				Score[4] = obj;
+				return true;
+			}
+			return false;
+		}
+
+		function sortAll ( obj) 
+		{
+
+			
+			//在进行排序
 			for ( var index = 0; index < 6 ; index ++)
 			{
 				for (var i = index + 1 ; i < 6 ; i ++)
@@ -55,29 +88,28 @@ wss.on('connection', function(ws){
 
 					if (Score[index].Score <= Score[i].Score)
 					{
-						if (Score[index].Name == Score[i].Name)
-						{
-							
-						}
-						else
-						{
-
+						
 						swap(index, i);
-						}
+						
 					}
 				}
 			}
 
 		}
-
-
 		//将data解析成对象
-		var GameScore = JSON.parse(data)
+		var GameScore = JSON.parse(data);
 		
-		sortAll(GameScore);
+		var needSort = isNeedSort(GameScore);
+		//是新的才进行排序,而且分数比较高的
 		str = JSON.stringify(Score);
-		console.log(str);
-	
+		sortAll(GameScore);
+		
+		//否则可能是老的，但是分数可能比较高的
+		str = JSON.stringify(Score);
+
 		ws.send(str);
+
+		//保存数据
+		fs.writeFileSync("./Score.json", str);
 	});
 });
